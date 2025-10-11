8c322cdbe6fd0aebfe8a86d70b3f033f00967553
LeyendaZero <aopenhimer@gmail.com>
2025-10-11 10:17:08 -0600

    Update seleccionar.lua

--- seleccionar.lua
+++ seleccionar.lua
@@ -190,7 +190,7 @@ end
 
 -- 🎨 Lista base de NPCs válidos
 local npcNames = {
-	"Nobini Pizzanini", "Gattatino Nyanino", "Matteo", "Espresso Signora", "Odin Din Din Dun",
+	"Noobini Pizzanini", "Gattatino Nyanino", "Matteo", "Espresso Signora", "Odin Din Din Dun",
 	"Statutino Libertino", "Ballerino Lololo", "Trigoligre Frutonni",
 	"Orcalero Orcala", "Los Crocodillitos", "Piccione Macchina",
 	"La Vacca Staturno Saturnita", "Chimpanzini Spiderini", "Los Tralaleritos",
